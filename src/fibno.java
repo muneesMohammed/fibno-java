ac6bcdc34f18d4f710f3257fb3e97b8a2175dc59
muneesMohammed <marhaba.munees@gmail.com>
2022-07-26 12:36:08 +0530

    addeded fibno class

---
+++ src/fibno.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class fibno {
+    public static void main(String[] args) {
+        import java.util.Scanner;
+
+        public class strinpalindrome {
+            public static void main(String[] args) {
+                int n = 1;
+                while (n > 0) {
+                    String word, reverse = "";
+                    System.out.println("enter a sring value");
+                    Scanner in = new Scanner(System.in);
+                    word = in.nextLine();
+                    int len = word.length();
+                    for (int i = len - 1; i >= 0; i--) {
+                        reverse = reverse + word.charAt(i);
+                    }
+                    if (word.equals(reverse)) {
+                        System.out.println("palindrome");
+                    } else {
+                        System.out.println("not palindrome");
+                    }
+                }
+            }
+        }
+    }
+}
